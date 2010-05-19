a3572de08ac2c4eee9e7b5e6ccefc03bd8658763
David Li <taweili@yahoo.com>
2010-05-19 16:48:28 +0800

    check for expired access token

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -18,3 +18,15 @@ YAHOO_OAUTH_CREDENTIALS = {
   :consumer_key => YAHOO_CONSUMER_KEY,
   :consumer_secret => YAHOO_SECRET_KEY
 } if !defined? YAHOO_OAUTH_CREDENTIALS
+
+begin
+  client = YahooOAuth::Client.new(:token => YAHOO_ACCESS_TOKEN, :secret => YAHOO_ACCESS_SECRET)
+  client.tinyusercard
+rescue OAuth::Problem
+  client = YahooOAuth::Client.new(:token => YAHOO_ACCESS_TOKEN, :secret => YAHOO_ACCESS_SECRET)
+  access_token = client.refresh_access_token
+  puts "Please update the token and secret in 'yahoo_config.rb'"
+  puts "YAHOO_ACCESS_TOKEN=\"#{access_token.token}\""
+  puts "YAHOO_ACCESS_SECRET=\"#{access_token.secret}\""
+  exit
+end
