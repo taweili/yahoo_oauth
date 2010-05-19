29f0015c3758b47f88ac6380c4a5ffef429e05f2
David Li <taweili@yahoo.com>
2010-05-19 11:50:01 +0800

    rewrite how credential is configured

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,4 +3,18 @@ require 'test/unit'
 require 'shoulda'
 
 require 'yahoo_oauth'
+
+#
+# Setup the Yahoo credentials and User token and secret to use with tests
+#
+# YAHOO_CONSUMER_KEY=""
+# YAHOO_SECRET_KEY=""
+# YAHOO_OAUTH_CALLBACK=""
+# YAHOO_ACCESS_TOKEN=""
+# YAHOO_ACCESS_SECRET=""
 require 'yahoo_config'
+
+YAHOO_OAUTH_CREDENTIALS = {
+  :consumer_key => YAHOO_CONSUMER_KEY,
+  :consumer_secret => YAHOO_SECRET_KEY
+}
